127fdc60300c0fecdfe81a76a0869e5d74acb75e
Sadeeptha Bandara <sadeeptha.bandara@gmail.com>
2024-07-21 19:08:35 +0530

    Abstracted data fetch logic to LaureateList component, Laureates Page component now only contains the intersection api logic and filter data logic

--- src/laureates/components/LaureateList.tsx
+++ src/laureates/components/LaureateList.tsx
@@ -1,32 +1,105 @@
-import { LaureateItemDetails } from "../../models/Laureate";
+import { forwardRef, useEffect, useImperativeHandle, useState } from "react";
+import { Laureate, LaureateItemDetails } from "../../models/Laureate";
 import Card from "../../shared/components/UIElements/Card";
 import LaureateItem from "./LaureateItem";
+import { FilterState } from "../pages/Laureates";
+import {
+  LAUREATES_FETCH_OFFSET,
+  getLaureates,
+} from "../../shared/api/nobel-api";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 
-const LaureateList = ({ items }: { items: LaureateItemDetails[] }) => {
-  if (items.length === 0) {
+export type LaureateListHandle = {
+  updateOffset: () => void;
+};
+
+type LaureateListProps = {
+  filterState: FilterState;
+};
+
+export const LaureateList = forwardRef<LaureateListHandle, LaureateListProps>(
+  ({ filterState }, ref) => {
+    const [offset, setOffset] = useState(0);
+    const [items, setItems] = useState<LaureateItemDetails[]>([]);
+    const [itemsLeft, setItemsLeft] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useImperativeHandle(ref, () => ({
+      updateOffset: () => {
+        // Data fetch already in progress/ Items fully fetched
+        if (isLoading || !itemsLeft) return;
+
+        setOffset((prev) => prev + LAUREATES_FETCH_OFFSET);
+      },
+    }));
+
+    // Reset offset and items when filter changes
+    useEffect(() => {
+      setOffset(0);
+      setItems([]);
+    }, [filterState]);
+
+    // When offset changes, trigger new api request
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          setIsLoading(true);
+          const data = await getLaureates(
+            offset,
+            FilterState.convertToObj(filterState)
+          );
+
+          const items = data.laureates
+            .filter((l: any) => l.knownName != undefined) // filter organizations
+            .map((l: any) => Laureate.fromJson(l).toLaureateItemDetails());
+
+          setItemsLeft(items.length > 0);
+          setItems((prev) => [...prev, ...items]);
+        } catch (error) {
+          console.log(error);
+        } finally {
+          setIsLoading(false);
+        }
+      };
+      fetchData();
+    }, [offset, filterState]);
+
+    if (items.length === 0) {
+      return (
+        <div className="m-10 flex items-center justify-center">
+          {isLoading ? (
+            <LoadingSpinner asOverlay />
+          ) : (
+            <>
+              <Card isFixedSize={true}>
+                <h2>Couldn't retrieve Laureates</h2>
+              </Card>
+            </>
+          )}
+        </div>
+      );
+    }
+
     return (
-      <div className="m-10 flex items-center justify-center">
-        <Card isFixedSize={true}>
-          <h2>Couldn't retrieve Laureates</h2>
-        </Card>
+      <div>
+        <ul className="my-0 mx-auto p-0 flex justify-center items-center flex-col first-line:flex-wrap">
+          {items.map((item, index) => (
+            <LaureateItem
+              key={index}
+              id={item.id}
+              knownName={item.knownName}
+              prizes={item.prizes}
+              birthYear={item.birthYear}
+              deathYear={item.deathYear}
+            />
+          ))}
+        </ul>
+        {isLoading && (
+          <div className="m-10 flex items-center justify-center">
+            <LoadingSpinner asOverlay />
+          </div>
+        )}
       </div>
     );
   }
-
-  return (
-    <ul className="my-0 mx-auto p-0 flex justify-center items-center flex-col first-line:flex-wrap">
-      {items.map((item, index) => (
-        <LaureateItem
-          key={index}
-          id={item.id}
-          knownName={item.knownName}
-          prizes={item.prizes}
-          birthYear={item.birthYear}
-          deathYear={item.deathYear}
-        />
-      ))}
-    </ul>
-  );
-};
-
-export default LaureateList;
+);
--- src/laureates/pages/Laureates.tsx
+++ src/laureates/pages/Laureates.tsx
@@ -1,16 +1,9 @@
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useRef, useState } from "react";
 
-import { Laureate, LaureateItemDetails } from "../../models/Laureate";
-import LaureateList from "../components/LaureateList";
+import { LaureateList, LaureateListHandle } from "../components/LaureateList";
 import FilteredSearch from "../../shared/components/UIElements/FilteredSearch";
 import Select from "../../shared/components/UIElements/Select";
-import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
-import {
-  GENDERS,
-  LAUREATES_FETCH_OFFSET,
-  NOBEL_PRIZE_CATEGORIES,
-  getLaureates,
-} from "../../shared/api/nobel-api";
+import { GENDERS, NOBEL_PRIZE_CATEGORIES } from "../../shared/api/nobel-api";
 
 // Currently supported filters
 const initialFilterState: FilterState = {
@@ -20,25 +13,19 @@ const initialFilterState: FilterState = {
 };
 
 const Laureates = () => {
-  const [offset, setOffset] = useState(0);
   const [filterState, setFilterState] = useState(initialFilterState);
-  const [items, setItems] = useState<LaureateItemDetails[]>([]);
-  const [itemsLeft, setItemsLeft] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
   const observer = useRef<IntersectionObserver | null>(null);
+  const childRef = useRef<LaureateListHandle>(null);
 
   // Using Intersection Observer API to implement data fetch on reaching end of page
   const setObserver = (node: HTMLDivElement) => {
-    // Data fetch already in progress
-    if (isLoading) return;
-
     // Remove previous observer
     if (observer.current) observer.current.disconnect();
 
     observer.current = new IntersectionObserver((entries) => {
       const entry = entries[0];
-      if (entry.isIntersecting && itemsLeft) {
-        setOffset((prev) => prev + LAUREATES_FETCH_OFFSET);
+      if (entry.isIntersecting && childRef.current) {
+        childRef.current.updateOffset();
       }
     });
 
@@ -46,37 +33,6 @@ const Laureates = () => {
       observer.current.observe(node);
     }
   };
-
-  // Reset offset and items when filter changes
-  useEffect(() => {
-    setOffset(0);
-    setItems([]);
-  }, [filterState]);
-
-  // When offset changes, trigger new api request
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        const data = await getLaureates(
-          offset,
-          FilterState.convertToObj(filterState)
-        );
-
-        const items = data.laureates
-          .filter((l: any) => l.knownName != undefined) // filter organizations
-          .map((l: any) => Laureate.fromJson(l).toLaureateItemDetails());
-
-        setItemsLeft(items.length > 0);
-        setItems((prev) => [...prev, ...items]);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchData();
-  }, [offset, filterState]);
 
   // Handle selection change
   const selectChangeHandler = useCallback((category: string, value: string) => {
@@ -108,13 +64,7 @@ const Laureates = () => {
           onChange={selectChangeHandler}
         />
       </div>
-      <LaureateList items={items} />
-
-      {isLoading && (
-        <div className="m-10 flex items-center justify-center">
-          <LoadingSpinner asOverlay />
-        </div>
-      )}
+      <LaureateList ref={childRef} filterState={filterState} />
       <div ref={setObserver}></div>
     </>
   );
@@ -129,7 +79,7 @@ enum FilterCategory {
 }
 
 // State type definitions
-class FilterState {
+export class FilterState {
   static convertToObj(state: FilterState) {
     return {
       gender: state.gender,
